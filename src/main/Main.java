80f3f36d2588d60203ff15d845b49b35c5cf39aa
= <=>
2023-10-17 12:39:22 +0200

    problemas para el carlos del futuro

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,25 @@ public class Main {
 	public static void main(String[] args) {
 		int nCoches = 10;
 		ArrayList<cocheHilo> coches = new ArrayList<cocheHilo>();
+		String[] carrera = new String[nCoches];
 		
 		for(int i = 0; i < nCoches; i++) {
 			coches.add(new cocheHilo(("C"+(i+1))));
-			
+			coches.get(i).start();
 		}
+		
+		for(cocheHilo coche : coches) {
+			try {
+				coche.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
 		
 		
 	}
--- src/main/cocheHilo.java
+++ src/main/cocheHilo.java
@@ -1,15 +1,32 @@
 package main;
 
+import java.util.Random;
+
 public class cocheHilo extends Thread{
 	private String id;
-	
+	private String[] posiNombre;
 	public cocheHilo(String id) {
-
+		
 		this.id = id;
 	}
 
-	public void run() {
+	public synchronized void run() {
+		Random r = new Random();
 		
+		int vuelta = 0;
+		
+		while(vuelta > 10) {
+			try {
+				Thread.sleep(r.nextLong(2500)+500);
+				vuelta++;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (vuelta == 10) {
+			
+		}
 	}
 	
 }
